19733cd6d65fe3491f73aae94151e2c01a89ccff
taylrfnt <taylr.fnt@gmail.com>
2025-05-07 20:47:49 -0500

    fujin: add modular foundation for desktop env&mgr

--- system/aarch64-linux.nix
+++ system/aarch64-linux.nix
@@ -38,12 +38,12 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
+  imports = [
+    ./desktop-environment/gnome.nix
+  ];
+
   # Enable spice-vdagent
   services.spice-vdagentd.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
---
+++ system/desktop-environment/kde-plasma.nix
@@ -0,0 +1,5 @@
+{
+  # Enable the KDE Plasma Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+}
