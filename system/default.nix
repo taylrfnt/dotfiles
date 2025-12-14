93f5ad92982837449ec444d7d708ec1b4592a222
taylrfnt <taylr.fnt@gmail.com>
2025-12-14 19:16:18 +0000

    system: enable tailscale service

--- system/default.nix
+++ system/default.nix
@@ -3,10 +3,11 @@
   lib,
   inputs,
   ...
-}: {
+}:
+{
   nix = {
     channel.enable = false;
-    registry = lib.mapAttrs (_: flake: {inherit flake;}) inputs;
+    registry = lib.mapAttrs (_: flake: { inherit flake; }) inputs;
     nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") inputs;
 
     settings = {
@@ -14,10 +15,13 @@
       flake-registry = "";
 
       download-buffer-size = 500000000;
-      experimental-features = ["nix-command" "flakes"];
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
       # enable community cache for modules
-      substituters = ["http://nix-community.cachix.org"];
-      trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
+      substituters = [ "http://nix-community.cachix.org" ];
+      trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
     };
   };
 
@@ -26,6 +30,12 @@
     variables = {
       EDITOR = "nvim";
     };
+  };
+
+  services.tailscale = {
+    enable = true;
+    # if you want a pre-authed key
+    # authKeyFile = "";
   };
 
   # enable zsh support
