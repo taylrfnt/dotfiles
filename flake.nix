b52e184bfae5980fa73b2d48a9191632ea24d9bd
taylrfnt <taylr.fnt@gmail.com>
2026-01-02 17:45:29 -0600

    chore: flake update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     hjem = {
-      url = "github:feel-co/hjem/refactor-3";
+      url = "github:feel-co/hjem";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
